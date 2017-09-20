3c6c48c874d39b7361185ae020b4d19bdae00500
Mateusz Przybylski <mordesku@gmail.com>
2017-09-20 17:30:05 +0200

    initial commit

---
+++ swing/src/main/java/pl/mordesku/sda/swing/WindowApplication.java
@@ -0,0 +1,13 @@
+package pl.mordesku.sda.swing;
+
+/**
+ * Created with notepad.exe.
+ * Author: mprzybylski
+ * Date: 2017-09-20
+ * Time: 17:28
+ */
+public class WindowApplication {
+    public static void main(String[] args) {
+        System.out.println("Application started");
+    }
+}
