6a557766212cec9927b5a1670cfb93b54bc99204
Mateusz Przybylski <mordesku@gmail.com>
2017-09-20 19:15:55 +0200

    swing

--- swing/src/main/java/pl/mordesku/sda/swing/WindowApplication.java
+++ swing/src/main/java/pl/mordesku/sda/swing/WindowApplication.java
@@ -1,5 +1,11 @@
 package pl.mordesku.sda.swing;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Created with notepad.exe.
  * Author: mprzybylski
@@ -9,5 +15,31 @@ package pl.mordesku.sda.swing;
 public class WindowApplication {
     public static void main(String[] args) {
         System.out.println("Application started");
+        JFrame window = new JFrame("SDA swing");
+        window.setSize(300,300);
+        window.setLayout(new BorderLayout());
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JButton button = new JButton("Załaduj plik");
+        window.add(button, BorderLayout.PAGE_START);
+        JTextField textField = new JTextField("test");
+        window.add(textField, BorderLayout.PAGE_END);
+        JTextArea textArea = new JTextArea();
+        window.add(textArea, BorderLayout.CENTER);
+        window.setVisible(true);
+
+        button.addActionListener(e -> {
+
+            System.out.println(textField.getText());
+            try {
+                textArea.setText(
+                        new String(
+                                Files.readAllBytes(
+                                        Paths.get(textField.getText()
+                                        )
+                                )));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
     }
 }
